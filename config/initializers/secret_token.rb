f9dcfa21892f751c487f8c670f4c09b90f6fac14
gitkpg <gitkpg@gmail.comm>
2013-07-19 00:31:15 +0400

    Fist_initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e39c5bba4bc0277060aa435000cf5144e2f5deedcf7044f6e60debcaaea8a8b817db2fe0612bd33ced864d555bcd46adfa32c857b14b67c089e477a5db66cbf2'
